f96af26ddcb12f4e5e5230bf76faeac6e3f90c26
Gr1nd5 <56499823+Gr1nd5@users.noreply.github.com>
2024-04-24 16:40:28 -0700

    Simple Intro dialogue
    Set up dialogue for intro clip to go into the game scene

--- Assets/Scripts/Dialogue/dialogueTrigger.cs
+++ Assets/Scripts/Dialogue/dialogueTrigger.cs
@@ -7,13 +7,37 @@ public class dialogueTrigger : MonoBehaviour
     [Header("Visual Cue")]
     [SerializeField] private GameObject visualCue;
 
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool userClicked;
+
     private void Awake()
     {
         visualCue.SetActive(false);
+        userClicked = false;
     }
 
-    void OnMouseDown()
+    private void Update()
     {
-        visualCue.SetActive(true);
+        if (userClicked && !DialogueManager.GetInstiance().dialogueIsPlaying)
+        {
+            visualCue.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                DialogueManager.GetInstiance().EnterDialogueMode(inkJSON);
+            }
+
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        userClicked = true;
     }
 }
